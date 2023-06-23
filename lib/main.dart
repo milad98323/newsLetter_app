2de60ec6d630080ecdaeca4a4be6bb923869f4ff
milad98323 <mosahosseini234@gmail.com>
2023-06-23 11:01:52 -0700

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+void toggleDrawer() {
+  if (_scaffoldKey.currentState?.isDrawerOpen ?? false) {
+    _scaffoldKey.currentState?.openEndDrawer();
+  } else {
+    _scaffoldKey.currentState?.openDrawer();
+  }
+}
+
+class DropDownList extends StatelessWidget {
+  const DropDownList({super.key, required this.name, required this.call});
+  final String name;
+  final Function call;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: ListTile(title: Text(name)),
+      onTap: () => call(),
+    );
+  }
+}
